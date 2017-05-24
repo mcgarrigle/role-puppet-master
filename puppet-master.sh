f815250f8b01461ce84b4d4d961bd2193c0496e2
root <root@puppet.foo.local>
2017-05-24 08:54:20 +0000

    added puppetserver config

--- puppet-master.sh
+++ puppet-master.sh
@@ -5,8 +5,8 @@ rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install -y puppet-agent
 yum install -y puppetserver
 
-vi /etc/sysconfig/puppetserver 
-vi /etc/puppetlabs/puppetserver/conf.d/puppetserver.conf 
+cp config/puppetserver      /etc/sysconfig/puppetserver
+cp config/puppetserver.conf /etc/puppetlabs/puppetserver/conf.d/puppetserver.conf
 
 systemctl enable puppetserver
 systemctl start puppetserver
@@ -14,4 +14,8 @@ puppet agent --test
 
 yum install -y puppetdb
 yum install -y puppetdb-termini
+
+yum install -y ruby
+gem install bundler
+gem install r10k
 
